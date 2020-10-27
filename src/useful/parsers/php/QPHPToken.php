91b0cd05d7a20de95f00b25909faffa06a6483a3
Alex Stanciu <ealexs@gmail.com>
2020-10-27 11:31:12 +0200

    update

--- src/useful/parsers/php/QPHPToken.php
+++ src/useful/parsers/php/QPHPToken.php
@@ -2461,6 +2461,7 @@ abstract class QPHPToken
 		{
 			// $tokens = qtoken_get_all(file_get_contents($path));
 			$readonly_tokens = $tokens = QPHPToken::GetPHPTokensFromFile($path);
+			
 			$file_without_php = "";
 			
 			$has_opener = false;
@@ -2494,7 +2495,10 @@ abstract class QPHPToken
 						"/ius", $file_without_php, $matches);
 
 					if (!$stat)
+					{
+						# qvar_dumpk($tokens);
 						throw new Exception("Parse error");
+					}
 					
 					$has_opener = false;
 					$has_closer = false;

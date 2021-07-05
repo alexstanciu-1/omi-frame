307d4c06d2c47cabc7277503720327c80ab65d10
Alex Stanciu <ealexs@gmail.com>
2021-07-05 11:38:40 +0300

    update

--- src/controller/QApp.php
+++ src/controller/QApp.php
@@ -93,8 +93,6 @@ class QApp extends QAppModule
 	 */
 	public static function Run($controllers = null)
 	{
-		\QSecurity_Check::Audit_Request();
-		
 		$request_uri = $_SERVER["REQUEST_URI"];
 		
 		$_return = null;
